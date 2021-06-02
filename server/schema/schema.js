9d3633065dbdc0190597066cc7e7ed0a319d3859
charles <charles@omnisis.co.uk>
2021-06-02 14:17:23 +0100

    add schemas

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,50 @@
 const graphql = require("graphql");
-const { buildResolveInfo } = require("graphql/execution/execute");
+const _ = require("lodash")
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+
+const books = [
+    { name: "Name of the Wind", genre: "Fantasy", id: "1", authorid: "1"},
+    { name: "The Final Empire", genre: "Fantasy", id: "2", authorid: "2"},
+    { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorid: "3"},
+    { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorid: "2"},
+    { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorid: "3"},
+    { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorid: "3"},
+]
+
+const authors = [
+    { name: "Pattrick Rothfuss", age: 44, id: "1" },
+    { name: "Brandom Sanderson", age: 44, id: "2" },
+    { name: "Terry Pratchet", age: 66, id: "3" },
+]
 
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: { 
+            type: AuthorType, 
+            resolve: (parent, args) => {
+                return _.find(authors, { id: parent.authorid });
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return _.filter(books, { authorid: parent.id });
+            }
+        }
     })
 });
 
@@ -17,13 +53,21 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // code to get data from db/other source
-                
+                return _.find(books, { id: args.id });
             }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // code to get data from db/other source
+                return _.find(authors, { id: args.id });
+            }  
         }
-    }
+    },
 });
 
 module.exports = new GraphQLSchema({
