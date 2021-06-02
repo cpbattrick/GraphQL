72f441e69b1043d97b867e79f3c78257290b614d
charles <charles@omnisis.co.uk>
2021-06-02 12:23:12 +0100

    add  inital schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,7 @@
 const graphql = require("graphql");
+const { buildResolveInfo } = require("graphql/execution/execute");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: "Book",
@@ -9,4 +10,22 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db/other source
+                
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
